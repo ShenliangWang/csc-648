59fde3aa07a1d109f0c8144af498a418fd33faa2
--quiet <fa17g06@csc648.c.sfsu-computer-science.internal>
2017-11-13 06:18:12 +0000

    Added express-validator framework to validate input and to sanitize them.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 var db = require('./db');
 
 var index = require('./routes/index');
@@ -21,6 +22,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(expressValidator());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
