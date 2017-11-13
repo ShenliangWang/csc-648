7f9f0023a73f00cf560274e492b9f8e52b5e0484
Felix <ofcfchin@gmail.com>
2017-11-12 17:31:40 -0800

    Created a new route /dbTest for use with backend testing of functions.

---
+++ routes/dbTest.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../db.js')
+
+/* GET home page. */
+router.get('/dbTest', function(req, res, next) {
+    res.render('dbTest', { title: 'tester' });
+});
+
+router.get('/test', function(req, res, next) {
+    db.search(req.query.search, (searchResults) => {
+        res.render('test', { title: 'Results Page', search: req.query.search, results: searchResults});
+    });
+});
+
+module.exports = router;
