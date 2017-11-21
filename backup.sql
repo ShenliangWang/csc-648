1584b2d187dc9b9485f5a96e70187a7aec075517
--quiet <fa17g06@csc648.c.sfsu-computer-science.internal>
2017-11-21 22:35:00 +0000

    sql dump file added

---
+++ backup.sql
@@ -0,0 +1,204 @@
+-- MySQL dump 10.13  Distrib 5.7.20, for Linux (x86_64)
+--
+-- Host: localhost    Database: fa17g06
+-- ------------------------------------------------------
+-- Server version	5.7.20-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Agents`
+--
+
+DROP TABLE IF EXISTS `Agents`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Agents` (
+  `agent_id` int(3) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(20) NOT NULL,
+  `lastname` varchar(20) NOT NULL,
+  `phonenumber` varchar(10) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`agent_id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Agents`
+--
+
+LOCK TABLES `Agents` WRITE;
+/*!40000 ALTER TABLE `Agents` DISABLE KEYS */;
+INSERT INTO `Agents` VALUES (1,'Jack ','Sparrow','4159953434','CptSparrow@essos.com','2017-11-12 13:30:00','2017-11-12 13:30:00');
+/*!40000 ALTER TABLE `Agents` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Users`
+--
+
+DROP TABLE IF EXISTS `Users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Users` (
+  `user_id` int(3) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `phoneNumber` varchar(10) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Users`
+--
+
+LOCK TABLES `Users` WRITE;
+/*!40000 ALTER TABLE `Users` DISABLE KEYS */;
+INSERT INTO `Users` VALUES (1,'Harley','Quinn','harleyAndMrJ@puddin.com','6502291745','2017-11-20 07:00:00','2017-11-20 07:00:00');
+/*!40000 ALTER TABLE `Users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `agent_password`
+--
+
+DROP TABLE IF EXISTS `agent_password`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `agent_password` (
+  `password_id` int(3) NOT NULL AUTO_INCREMENT,
+  `agent_email` varchar(40) NOT NULL,
+  `agent_password` varchar(255) NOT NULL,
+  PRIMARY KEY (`password_id`),
+  KEY `agent_email` (`agent_email`),
+  CONSTRAINT `agent_password_ibfk_1` FOREIGN KEY (`agent_email`) REFERENCES `Agents` (`email`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `agent_password`
+--
+
+LOCK TABLES `agent_password` WRITE;
+/*!40000 ALTER TABLE `agent_password` DISABLE KEYS */;
+INSERT INTO `agent_password` VALUES (1,'CptSparrow@essos.com','TheBlackPearl');
+/*!40000 ALTER TABLE `agent_password` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `image`
+--
+
+DROP TABLE IF EXISTS `image`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `image` (
+  `image_pk` int(3) NOT NULL AUTO_INCREMENT,
+  `listing_id` int(3) NOT NULL,
+  `image_path` varchar(40) NOT NULL,
+  PRIMARY KEY (`image_pk`),
+  KEY `listing_id` (`listing_id`),
+  CONSTRAINT `image_ibfk_1` FOREIGN KEY (`listing_id`) REFERENCES `listings` (`listing_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `image`
+--
+
+LOCK TABLES `image` WRITE;
+/*!40000 ALTER TABLE `image` DISABLE KEYS */;
+INSERT INTO `image` VALUES (1,1,'/images/listing_id_1/1.jpeg'),(2,2,'/images/listing_id_2/1.jpeg'),(3,3,'/images/listing_id_3/1.jpeg'),(4,4,'/images/listing_id_4/1.jpeg'),(5,5,'/images/listing_id_5/1.jpeg');
+/*!40000 ALTER TABLE `image` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `listings`
+--
+
+DROP TABLE IF EXISTS `listings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `listings` (
+  `listing_id` int(3) NOT NULL AUTO_INCREMENT,
+  `type` int(2) NOT NULL,
+  `sqrft` int(7) NOT NULL,
+  `price` int(10) NOT NULL,
+  `city` varchar(20) NOT NULL,
+  `state` varchar(15) NOT NULL,
+  `zipcode` int(10) NOT NULL,
+  `address` varchar(40) NOT NULL,
+  `bedrooms` int(2) NOT NULL,
+  `bathrooms` int(2) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `agent_id` int(3) NOT NULL,
+  PRIMARY KEY (`listing_id`),
+  KEY `agent_id` (`agent_id`),
+  CONSTRAINT `listings_ibfk_1` FOREIGN KEY (`agent_id`) REFERENCES `Agents` (`agent_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `listings`
+--
+
+LOCK TABLES `listings` WRITE;
+/*!40000 ALTER TABLE `listings` DISABLE KEYS */;
+INSERT INTO `listings` VALUES (1,1,1500,1000000,'San Francisco','CA',94112,'50 Phelan Ave',3,2,'A cozy home nestled in the rolling fog of the city by the bay.',1),(2,1,1750,1500000,'San Francisco','CA',94102,'1 Dr Carlton B Goodlett Pl',3,2,'Positioned near city hall, you\'ll never be closer to the levers of power!',1),(3,1,2000,2000000,'San Francisco','CA',94102,'200 Larkin Street',4,2,'A cultural hub with treasures to be had for the daring!',1),(4,1,4000,5000000,'San Francisco','CA',94102,'99 Grove Street',8,4,'A suitable site for a grand conclave of irreputable gentleman!',1),(5,1,3000,2500000,'San Francisco','CA',94111,'One Ferry Building',4,2,'Space to park a car or a pirate ship! Yar!',1);
+/*!40000 ALTER TABLE `listings` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_password`
+--
+
+DROP TABLE IF EXISTS `user_password`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_password` (
+  `password_id` int(3) NOT NULL AUTO_INCREMENT,
+  `user_email` varchar(255) NOT NULL,
+  `user_password` varchar(255) NOT NULL,
+  PRIMARY KEY (`password_id`),
+  KEY `user_email` (`user_email`),
+  CONSTRAINT `user_password_ibfk_1` FOREIGN KEY (`user_email`) REFERENCES `Users` (`email`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_password`
+--
+
+LOCK TABLES `user_password` WRITE;
+/*!40000 ALTER TABLE `user_password` DISABLE KEYS */;
+INSERT INTO `user_password` VALUES (1,'harleyAndMrJ@puddin.com','notSuchASecretPasswordBatsy');
+/*!40000 ALTER TABLE `user_password` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-11-21 22:31:13
