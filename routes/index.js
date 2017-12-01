f217eb852ba7d9d2e4630497bb9b298209187250
--quiet <fa17g06@csc648.c.sfsu-computer-science.internal>
2017-12-01 21:20:01 +0000

    Changed variable Listings to db for clarity

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const  Listings  = require('../db');
+const  db  = require('../db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -17,7 +17,7 @@ router.get('/test', function(req, res, next) {
     req.check('search','Invalid search input').escape(req.query.search);//Sanatize the input by escaping harmful chars --Required
 	//req.check('search','Invalid search input').isAlphanumeric(req.query.search,[en-US]);//checks if the string is alpha-numeric and US chars --helpful
     
-    Listings.Listings.search(req.query.search, (searchResults) => {
+    db.Listings.search(req.query.search, (searchResults) => {
         res.render('test', { title: 'Results Page', search: req.query.search, results: searchResults, search_val: req.query.search});
     });
 });
