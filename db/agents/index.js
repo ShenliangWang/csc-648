63198f14c5981da0ad6e10cd304142f463b01b06
Sockshare <julianmorrisette2012@gmail.com>
2017-12-01 14:34:00 -0800

    Updating Agents
    Changed all the functions.  They now reflect what is inside of the MYSQL_QUERIES folder

--- db/agents/index.js
+++ db/agents/index.js
@@ -8,87 +8,98 @@ var del = "DELETE FROM " + table + " ";
 
 /* insert new agent to agents table in db 
    Note: Testing required.
-   @author: Felix. 			*/
+   @author: Felix + Julian. 			*/
 function create_agent(fname, lname, phone, email, pwd, callback) {
-	var mysqlTimestamp = moment(Date.now()).format('YYYY-MM-DD HH:mm:ss');
 
-	db.query(  insert + 
-		   "(firstname, lastname, phonenumber, email, created_at, updated_at) " + 
-		     "VALUES " + 
-		     "(" + mysql.escape(fname) + "," + mysql.escape(lname) + "," + mysql.escape(phone)
-		     + "," + mysql.escape(email) +"," + mysqlTimestamp + "," + mysqlTimestamp +  ")",
-		     function(err, agent) {
-			     if(err) {
-				res.status(500).json({"status_code": 500,"status_message": "internal server error"});
-				return callback(err);
-			    } else {
-				   return callback(agent); 
-			     }
-		     }
-	)
-
-	//Todo: Insert into passwords table given password for this agent.
+var mysqlTimestamp = moment(Date.now()).format('YYYY-MM-DD HH:mm:ss');
+ db.query("INSERT INTO Agents(firstname, lastname, phonenumber, email) VALUES ("+mysql.escape(fname)+","+mysql.escape(lname)+", "+mysql.escape(phone)+", "+mysql.escape(email)+","+mysqlTimestamp+")", 
+    function(err,rows,fields) {
+        if(err) {
+            res.status(500).json({"status_code": 500,"status_message": "internal server error"});
+            return callback(err);
+        }
+    })
+	
+	 db.query("INSERT INTO password(email, password) VALUES ("+mysql.escape(email)+", "+mysql.escape(password)+")", 
+    function(err,rows,fields) {
+        if(err) {
+            res.status(500).json({"status_code": 500,"status_message": "internal server error"});
+            return callback(err);
+        }
+    })
 }
 
 
 /* Gets agent from db table agents
    Note: Testing required.
-   @author: Felix		*/
+   @author: Felix + Julian		*/
+
 function get_agent(agent_id, callback) {
-	db.query( select + 
-		 "WHERE agent_id= " + mysql.escape(agent_id), //built in mysql escape to prevent sql ijections
-		function(err, agent) {
-			if(err) {
-				res.status(500).json({"status_code": 500,"status_message": "internal server error"});
-				return callback(err);
-			} else {
-				return callback(agent);
-			}
-		}
-	)
-}
+    db.query("SELECT * FROM Agents WHERE agent_id = " + mysql.escape(agent_id) +"", 
+    function(err,rows,fields) {
+        if(err) {
+            res.status(500).json({"status_code": 500,"status_message": "internal server error"});
+            return callback(err);
+        } else {
+            var agents = [];
+            for(var i = 0; i < rows.length; i++) {
+
+                var agent = {
+		    'agent_id' : rows[i].agent_id,
+                    'firstname' : rows[i].firstname,
+                    'lastname' : rows[i].lastname,
+                    'phonenumber' : rows[i].zipcode,
+                    'email' : rows[i].email
+                }
+                agents.push(agent);
+            }
+            return callback(agents);
+        }})}
 
 
 /* Update selected agent's phone from db table agents. 
    Note: Testing req'd
-   @author: Felix					*/
-function set_phone(agent_id, phone, callback) {
-	db.query(update + 
-		 "SET phonenumber = " + mysql.escape(phone) + //built in mysql escape to prevent sql ijections
-		 "WHERE agent_id = " + agent_id,
-		 function(err, result) {
-			if(err){
-				res.status(500).json({"status_code": 500,"status_message": "internal server error"});
-				return callback(err);
-			} else {
-				return callback(result);
-			}
-		}
-	)
-}
+   @author: Felix + Julian   */
+//This function updates the Agent's phone number using their ID
+function set_phone(agent_id,phone,callback) {
+	db.query("UPDATE TABLE Agents SET phonenumber = "+mysql.escape(phone)+" WHERE agent_id = "+mysql.escape(agent_id)+")", 
+    function(err,rows,fields) {
+        if(err) {
+            res.status(500).json({"status_code": 500,"status_message": "internal server error"});
+            return callback(err);
+        }})}
+//This function updates the Agent's phone number using their email
+function set_phone(agent_email,phone,callback) {
+	db.query("UPDATE TABLE Agents SET phonenumber = "+mysql.escape(phone)+" WHERE email = "+mysql.escape(agent_email)+")", 
+    function(err,rows,fields) {
+        if(err) {
+            res.status(500).json({"status_code": 500,"status_message": "internal server error"});
+            return callback(err);
+        }})}
+
+
 
 /* Update selected agent's email from db table agents. 
    Note: Testing req'd
-   @author: Felix					*/
-function set_email(agent_id, email, callback) {
-        db.query(update + 
-		"SET email = " + mysql.escape(email) + //built in mysql escape to prevent sql ijections
-		"WHERE agent_id = " + agent_id,
-		function(err, result) {
-		       if(err){
-			       res.status(500).json({"status_code": 500,"status_message": "internal server error"});
-			       return callback(err);
-		       } else {
-			       return callback(result);
-		       }
-	       }
-	)
-}
+   @author: Felix + Julian  */
+
+
+function set_email(agent_id,email,callback) {
+	db.query("UPDATE TABLE Agents SET email = "+mysql.escape(email)+" WHERE agent_id = "+mysql.escape(agent_id)+")", 
+    function(err,rows,fields) {
+        if(err) {
+            res.status(500).json({"status_code": 500,"status_message": "internal server error"});
+            return callback(err);
+        }})}
 
 /* Update selected agent's password from db table agents. */
-function set_password(agent_id, pword, callback) {
-        //Needs implementation
-}
+function set_password(agent_email,pword,callback) {
+	db.query("UPDATE TABLE password SET password = "+mysql.escape(pword)+" WHERE email = "+mysql.escape(agent_email)+")", 
+    function(err,rows,fields) {
+        if(err) {
+            res.status(500).json({"status_code": 500,"status_message": "internal server error"});
+            return callback(err);
+        }})}
 
 
 
