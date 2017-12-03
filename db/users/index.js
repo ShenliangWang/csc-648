11cdc738b94ac1a7fba431caebf2485e1370ecb3
Sockshare <julianmorrisette2012@gmail.com>
2017-12-03 12:49:05 -0800

    Updating Users page
    Here I am making it so the queries match up with the MYSQL_QUERIES page for Users

--- db/users/index.js
+++ db/users/index.js
@@ -8,122 +8,131 @@ var del = "DELETE FROM " + table + " ";
 
 /* insert new user to users table in db 
    Note: Testing required.
-   @author: Felix. 			*/
-   function create_user(fname, lname, phone, email, pwd, callback) {
-	var mysqlTimestamp = moment(Date.now()).format('YYYY-MM-DD HH:mm:ss');
+   @author: Felix. 	+ Julian		*/
+  
 
-	db.query(  insert + 
-		   "(firstname, lastname, phonenumber, email, created_at, updated_at) " + 
-		     "VALUES " + 
-		     "(" + mysql.escape(fname) + "," + mysql.escape(lname) + "," + mysql.escape(phone)
-		     + "," + mysql.escape(email) +"," + mysqlTimestamp + "," + mysqlTimestamp +  ")",
-		     function(err, user) {
-			     if(err) {
-				res.status(500).json({"status_code": 500,"status_message": "internal server error"});
-				return callback(err);
-			    } else {
-				   return callback(user); 
-			     }
-		     }
-	)
+function create_user(fname, lname, phone, email, pwd, callback) {
+var mysqlTimestamp = moment(Date.now()).format('YYYY-MM-DD HH:mm:ss');
 
-	//Todo: Insert into passwords table given password for this user.
-}
+ db.query("INSERT INTO Users(firstname, lastname, phonenumber, email, createdAt) VALUES ("+mysql.escape(fname)+", "+mysql.escape(lname)+", "+mysql.escape(phone)+", "+mysql.escape(email)+", "+mysqlTimestamp+")", 
+    function(err,rows,fields) {
+        if(err) {
+            res.status(500).json({"status_code": 500,"status_message": "internal server error"});
+            return callback(err);
+        }})
+	
+	 db.query("INSERT INTO password(email, password) VALUES ("+mysql.escape(email)+", "+mysql.escape(password)+")", 
+    function(err,rows,fields) {
+        if(err) {
+            res.status(500).json({"status_code": 500,"status_message": "internal server error"});
+            return callback(err);
+        }})}
 
 
 /* Gets user from db table user
    Note: Testing required.
-   @author: Felix. 		*/
+   @author: Felix + Julian*/
 function get_user(user_id, callback) {
-        db.query( select + 
-		"WHERE user_id= " + mysql.escape(user_id), //built in mysql escape to prevent sql ijections
-	       function(err, user) {
-		       if(err) {
-			       res.status(500).json({"status_code": 500,"status_message": "internal server error"});
-			       return callback(err);
-		       } else {
-			       return callback(user);
-		       }
-	       }
-       )
-}
+    db.query("SELECT * FROM Users WHERE user_id = " + mysql.escape(user_id) +"", 
+    function(err,rows,fields) {
+        if(err) {
+            res.status(500).json({"status_code": 500,"status_message": "internal server error"});
+            return callback(err);
+        } else {
+            var users = [];
+            for(var i = 0; i < rows.length; i++) {
+                var user = {
+		    'user_id' : rows[i].user_id,
+                    'firstname' : rows[i].firstname,
+                    'lastname' : rows[i].lastname,
+                    'phonenumber' : rows[i].zipcode,
+                    'email' : rows[i].email
+                }
+                users.push(user);
+            }
+            return callback(users);
+        }})}
 
 /* Update selected users's fname from db table users.
    Note: Testing required.
-   @author: Felix. 					 */
-function set_fname(user_id, fname, callback) {
-        db.query( update +
-		"SET fname = " + mysql.escape(fname) + //built in mysql escape to prevent sql ijections
-	       " WHERE user_id = " + user_id,
-	      function(err, result) {
-		      if(err){
-			      res.status(500).json({"status_code": 500,"status_message": "internal server error"});
-			      return callback(err);
-		      } else {
-			      return callback(result);
-		      }
-	      }
-      )
-}
+   @author: Felix. + Julian */
+
+function set_fname(user_id,fname,callback) {
+	db.query("UPDATE TABLE Users SET firstname = "+mysql.escape(fname)+" WHERE user_id = "+mysql.escape(user_id)+")", 
+    function(err,rows,fields) {
+        if(err) {
+            res.status(500).json({"status_code": 500,"status_message": "internal server error"});
+            return callback(err);
+        }})}
+
+function set_fname(user_email,fname,callback) {
+	db.query("UPDATE TABLE Users SET firstname = "+mysql.escape(fname)+" WHERE email = "+mysql.escape(user_email)+")", 
+    function(err,rows,fields) {
+        if(err) {
+            res.status(500).json({"status_code": 500,"status_message": "internal server error"});
+            return callback(err);
+        }})}
 
 /* Update selected user's lname from db table userts.
    Note: Testing required.
-   @author: Felix. 					 */
+   @author: Felix. + Julian */
 function set_lname(user_id, lname, callback) {
-        db.query( update +
-		"SET fname = " + mysql.escape(lname) + //built in mysql escape to prevent sql ijections
-	       " WHERE user_id = " + user_id,
-	      function(err, result) {
-		      if(err){
-			      res.status(500).json({"status_code": 500,"status_message": "internal server error"});
-			      return callback(err);
-		      } else {
-			      return callback(result);
-		      }
-	      }
-	)
-}
+	db.query("UPDATE TABLE Users SET lastname = "+mysql.escape(lname)+" WHERE user_id = "+mysql.escape(user_id)+")", 
+    function(err,rows,fields) {
+        if(err) {
+            res.status(500).json({"status_code": 500,"status_message": "internal server error"});
+            return callback(err);
+        }})}
+
+function set_lname(user_email,lname,callback) {
+	db.query("UPDATE TABLE Users SET lastname = "+mysql.escape(lname)+" WHERE email = "+mysql.escape(user_email)+")", 
+    function(err,rows,fields) {
+        if(err) {
+            res.status(500).json({"status_code": 500,"status_message": "internal server error"});
+            return callback(err);
+        }})}
 
 /* Update selected user's phone from db table users.
    Note: Testing required.
    @author: Felix. 				 */
-function set_phone(user_id, phone, callback) {
-        db.query(update + 
-		"SET phonenumber = " + mysql.escape(phone) + //built in mysql escape to prevent sql ijections
-		"WHERE user_id = " + user_id,
-		function(err, result) {
-		       if(err){
-			       res.status(500).json({"status_code": 500,"status_message": "internal server error"});
-			       return callback(err);
-		       } else {
-			       return callback(result);
-		       }
-	       }
-       )
-}
+function set_phone(user_id,phone,callback) {
+	db.query("UPDATE TABLE Users SET phonenumber = "+mysql.escape(phone)+" WHERE user_id = "+mysql.escape(user_id)+")", 
+    function(err,rows,fields) {
+        if(err) {
+            res.status(500).json({"status_code": 500,"status_message": "internal server error"});
+            return callback(err);
+        }})}
+
+function set_phone(user_email,phone,callback) {
+	db.query("UPDATE TABLE Users SET phonenumber = "+mysql.escape(phone)+" WHERE email = "+mysql.escape(user_email)+")", 
+    function(err,rows,fields) {
+        if(err) {
+            res.status(500).json({"status_code": 500,"status_message": "internal server error"});
+            return callback(err);
+        }})}
+
 
 /* Update selected user's email from db table users.
    Note: Testing required.
-   @author: Felix. 				 */
-function set_email(users_id, email, callback) {
-        db.query(update + 
-		"SET email = " + mysql.escape(email) + //built in mysql escape to prevent sql ijections
-		"WHERE user_id = " + user_id,
-		function(err, result) {
-		       if(err){
-			       res.status(500).json({"status_code": 500,"status_message": "internal server error"});
-			       return callback(err);
-		       } else {
-			       return callback(result);
-		       }
-	       }
-	)
-}
+   @author: Felix. + Julian */
+function set_email(user_id,email,callback) {
+	db.query("UPDATE TABLE Users SET email = "+mysql.escape(email)+" WHERE user_id = "+mysql.escape(agent_id)+")", 
+    function(err,rows,fields) {
+        if(err) {
+            res.status(500).json({"status_code": 500,"status_message": "internal server error"});
+            return callback(err);
+        }})}
 
-/* Update selected users's password from db table userss. */
-function set_password(users_id, pword, callback) {
-        //Needs implementation
-}
+/* Update selected users's password from db table userss. 
+  @author: Felix. + Julian 
+*/
+function set_password(user_email,pword,callback) {
+	db.query("UPDATE TABLE password SET password = "+mysql.escape(pword)+" WHERE email = "+mysql.escape(user_email)+")", 
+    function(err,rows,fields) {
+        if(err) {
+            res.status(500).json({"status_code": 500,"status_message": "internal server error"});
+            return callback(err);
+        }})}
 
 
 
