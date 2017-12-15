6bd07179ba63c38c90f1f87573ee7883e3b91ed2
Felix <ofcfchin@gmail.com>
2017-12-15 14:10:21 -0800

    Changed fcns to auto escape user entered values by placing them into a array. Previously mysql.escape was used on user entered values.

--- db/agents/index.js
+++ db/agents/index.js
@@ -12,7 +12,8 @@ var del = "DELETE FROM " + table + " ";
 function create_agent(fname, lname, phone, email, pwd, callback) {
 
 var mysqlTimestamp = moment(Date.now()).format('YYYY-MM-DD HH:mm:ss');
- db.query("INSERT INTO Agents(firstname, lastname, phonenumber, email, created_at) VALUES ("+mysql.escape(fname)+","+mysql.escape(lname)+", "+mysql.escape(phone)+", "+mysql.escape(email)+","+mysqlTimestamp+")", 
+
+ db.query("INSERT INTO Agents(firstname, lastname, phonenumber, email, created_at) VALUES (?, ?, ?, ?, ?)", [fname, lname, phone, email], 
     function(err,rows,fields) {
         if(err) {
             res.status(500).json({"status_code": 500,"status_message": "internal server error"});
@@ -20,7 +21,7 @@ var mysqlTimestamp = moment(Date.now()).format('YYYY-MM-DD HH:mm:ss');
         }
     })
 	
-	 db.query("INSERT INTO password(email, password) VALUES ("+mysql.escape(email)+", "+mysql.escape(password)+")", 
+	 db.query("INSERT INTO password(email, password) VALUES (?, ?)", [email, pwd], 
     function(err,rows,fields) {
         if(err) {
             res.status(500).json({"status_code": 500,"status_message": "internal server error"});
@@ -35,7 +36,8 @@ var mysqlTimestamp = moment(Date.now()).format('YYYY-MM-DD HH:mm:ss');
    @author: Felix + Julian		*/
 
 function get_agent(agent_id, callback) {
-    db.query("SELECT * FROM Agents WHERE agent_id = " + mysql.escape(agent_id) +"", 
+
+    db.query("SELECT * FROM Agents WHERE agent_id = ?", [agent_id], 
     function(err,rows,fields) {
         if(err) {
             res.status(500).json({"status_code": 500,"status_message": "internal server error"});
@@ -62,7 +64,7 @@ function get_agent(agent_id, callback) {
    @author: Felix + Julian   */
 //This function updates the Agent's phone number using their ID
 function set_phone(agent_id,phone,callback) {
-	db.query("UPDATE TABLE Agents SET phonenumber = "+mysql.escape(phone)+" WHERE agent_id = "+mysql.escape(agent_id)+")", 
+	db.query("UPDATE TABLE Agents SET phonenumber = ? WHERE agent_id = ?", [phone, agent_id], 
     function(err,rows,fields) {
         if(err) {
             res.status(500).json({"status_code": 500,"status_message": "internal server error"});
@@ -70,7 +72,7 @@ function set_phone(agent_id,phone,callback) {
         }})}
 //This function updates the Agent's phone number using their email
 function set_phone(agent_email,phone,callback) {
-	db.query("UPDATE TABLE Agents SET phonenumber = "+mysql.escape(phone)+" WHERE email = "+mysql.escape(agent_email)+")", 
+	db.query("UPDATE TABLE Agents SET phonenumber = ? WHERE email = ?", [phone, agent_email], 
     function(err,rows,fields) {
         if(err) {
             res.status(500).json({"status_code": 500,"status_message": "internal server error"});
@@ -85,7 +87,7 @@ function set_phone(agent_email,phone,callback) {
 
 
 function set_email(agent_id,email,callback) {
-	db.query("UPDATE TABLE Agents SET email = "+mysql.escape(email)+" WHERE agent_id = "+mysql.escape(agent_id)+")", 
+	db.query("UPDATE TABLE Agents SET email = ? WHERE agent_id = ?", [email, agent_id], 
     function(err,rows,fields) {
         if(err) {
             res.status(500).json({"status_code": 500,"status_message": "internal server error"});
@@ -94,7 +96,7 @@ function set_email(agent_id,email,callback) {
 
 /* Update selected agent's password from db table agents. */
 function set_password(agent_email,pword,callback) {
-	db.query("UPDATE TABLE password SET password = "+mysql.escape(pword)+" WHERE email = "+mysql.escape(agent_email)+")", 
+	db.query("UPDATE TABLE password SET password = ? WHERE email = ?" [pword, agent_email], 
     function(err,rows,fields) {
         if(err) {
             res.status(500).json({"status_code": 500,"status_message": "internal server error"});
